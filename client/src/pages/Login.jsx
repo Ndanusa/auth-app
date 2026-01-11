9c830c9274b983ac180cbcd1c892644a7f47f2ef
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-11 23:41:20 +0100

    this is alte

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import logoSrc from "../assets/logo.svg";
 function Login() {
-   return <></>;
+   return (
+      <>
+         <div className="flex h-screen items-center justify-around">
+            <div className="">This is a test for the section</div>
+            <div className="bg-black text-white flex flex-col items-center justify-center px-10 py-10">
+               <img src={logoSrc} alt="" className="w-100" />
+               This is a test for the section
+            </div>
+         </div>
+      </>
+   );
 }
 
 export default Login;
