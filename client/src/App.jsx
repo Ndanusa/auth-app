4472d65f7406f0df2a19c51029a31e694e78cd6f
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-11 22:27:38 +0100

    pussy bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+
 function App() {
-   const [count, setCount] = useState("");
-   async function getUUID() {
-      const response = await fetch("http://localhost:4000");
-      const data = await response.json();
-      setCount(data)
-   }
    return (
-      <>
-         <div>
-            <button onClick={getUUID}>Get uuid</button>
-            {count}
-         </div>
-      </>
+      <Routes>
+         <Route path="/" element={<Navigate to="/login" />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/signup" element={<Signup />} />
+         <Route path="/home" element={<Home />} />
+      </Routes>
    );
 }
 
