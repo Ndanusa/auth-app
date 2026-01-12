a4d2b0802af6b4f4969a7fcd09a0f9da843c7f07
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-12 02:39:50 +0100

    created the form sectipon

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,113 @@
 import { Link } from "react-router-dom";
-import logoSrc from "../assets/logo-light.svg";
+import logoSrcLight from "../assets/logo-light.svg";
+import logoSrcDark from "../assets/logo.svg";
+import { useState } from "react";
+import { Mail, Key } from "lucide-react";
 function Login() {
+   const [emailError, setEmailError] = useState("");
+   const [passwordError, setPasswordError] = useState("");
+   const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/;
+
    return (
       <>
          <div className="flex h-screen items-center justify-between px-30">
-            <div className="">This is a test for the section</div>
+            <div className="">
+               <div className="w-16 flex flex-col gap-3">
+                  <img src={logoSrcDark} alt="" />
+                  <p className="font-semibold">A.Vibe</p>
+               </div>
+               <div className="form my-7">
+                  <h1 className="text-2xl font-bold">Sign in</h1>
+                  <div className="py-10">
+                     <div>
+                        <label htmlFor="email">
+                           <p
+                              className={`text-xs font-semibold ${
+                                 emailError !== "" ? "text-red-600" : ""
+                              }`}>
+                              Email Address
+                           </p>
+                        </label>
+                        <div className="relative flex items-center">
+                           <span className="absolute bottom-2 left-2">
+                              <Mail
+                                 size={18}
+                                 color={
+                                    emailError !== "" ? "#ff0000" : "#5c5c5c"
+                                 }
+                                 strokeWidth={2.25}
+                              />
+                           </span>
+                           <input
+                              className={`placeholder:text-xs text-sm pl-8 bg-white py-2 px-3 sqc-lg mt-2 w-90 ${
+                                 emailError !== ""
+                                    ? "text-red-600 focus:outline-0 border-2 border-red-600 placeholder:text-red-500"
+                                    : "text-zinc-900 focus:outline-2 border-0 placeholder:text-zinc-500"
+                              }`}
+                              type="email"
+                              name="email"
+                              placeholder="Email address"
+                              onChange={(e) => {
+                                 if (emailRegex.test(e.target.value.trim())) {
+                                    setEmailError("");
+                                 } else {
+                                    setEmailError("Email address is not valid");
+                                 }
+                              }}
+                           />
+                        </div>
+
+                        <p className="text-red-600 text-xs font-normal">
+                           {emailError}
+                        </p>
+                     </div>
+                     <div className="mt-7">
+                        <label htmlFor="email">
+                           <p
+                              className={`text-xs font-semibold ${
+                                 passwordError !== "" ? "text-red-600" : ""
+                              }`}>
+                              Password
+                           </p>
+                        </label>
+                        <div className="relative flex items-center">
+                           <span className="absolute bottom-2 left-2">
+                              <Key
+                                 size={18}
+                                 color={
+                                    passwordError !== "" ? "#ff0000" : "#5c5c5c"
+                                 }
+                                 strokeWidth={2.25}
+                              />
+                           </span>
+                           <input
+                              className={`placeholder:text-xs placeholder:text-zinc-500 text-sm pl-8 bg-white py-2 px-3 sqc-lg mt-2 w-90 ${
+                                 passwordError !== ""
+                                    ? "text-red-600 focus:outline-0 border-2 border-red-600"
+                                    : "text-zinc-900 focus:outline-2 border-0"
+                              }`}
+                              type="password"
+                              name="password"
+                              placeholder="Password"
+                              onChange={(e) => {
+                                 const length = e.target.value.length;
+                                 if (length < 8) {
+                                    setPasswordError(
+                                       "Password must be 8 characters or more"
+                                    );
+                                 } else setPasswordError("");
+                              }}
+                           />
+                        </div>
+
+                        <p className="text-red-600 text-xs font-normal">
+                           {passwordError}
+                        </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
             <div className="bg-zinc-900 rounded-3xl sqc-2xl p-6">
                <div className="relative max-w-md w-full  p-8 overflow-hidden mx-auto">
                   <div className="absolute top-0 right-0 w-32 h-32 border-r-4 border-t-4 border-gray-700 rounded-tr-3xl sqc-2xl-tr"></div>
@@ -12,7 +115,7 @@ function Login() {
 
                   <div className="mb-8">
                      <div className="w-52 h-32 mb-4">
-                        <img src={logoSrc} alt="" />
+                        <img src={logoSrcLight} alt="" />
                      </div>
                      <h2 className="text-white text-sm font-medium">A.Vibe</h2>
                   </div>
