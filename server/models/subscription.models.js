b67b59c3c4c2e06507c9b4860a4a17bd1d8263aa
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-13 03:56:13 +0100

    subscriptions controller for server side

--- server/models/subscription.models.js
+++ server/models/subscription.models.js
@@ -1,3 +1,68 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({ timestamps: true });
+const subscriptionSchema = new mongoose.Schema(
+   {
+      name: {
+         type: String,
+         required: [true, "Subscription name is required"],
+         trim: true,
+         minLength: 2,
+         maxLength: 100,
+      },
+      price: {
+         type: Number,
+         required: [true, "Subscription proce s required"],
+         min: [0, "price must be greater than 0"],
+      },
+      currenct: {
+         type: String,
+         enum: ["NGN", "USD", "EUR", "GBP"],
+         default: "NGN",
+      },
+      frequency: {
+         type: String,
+         enum: ["daily", "weekly", "monthly", "yearly"],
+      },
+      category: {
+         type: String,
+         enum: [
+            "entertainment",
+            "sports",
+            "news",
+            "gaming",
+            "lufestyle",
+            "finance",
+            "politics",
+            "other",
+         ],
+         required: [true, "Categoty is required"],
+      },
+      paymentMethod: {
+         type: String,
+         required: true,
+         trim: true,
+      },
+      status: {
+         type: String,
+         required: true,
+         enum: ["active", "expired", "cancelled"],
+      },
+      startDate: {
+         type: Date,
+         required: true,
+         validate: {
+            validator(value) {
+               return value > this.startDate;
+            },
+            message: "Start date must not be in the past",
+         },
+      },
+      user: {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "User",
+         required: true,
+         index: true,
+      },
+   },
+   { timestamps: true }
+);
