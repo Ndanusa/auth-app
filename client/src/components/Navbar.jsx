396672bcef0ef68ad12e43b9d65d4b1b61190d68
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-18 14:57:55 +0100

    added the divider the navbar logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 function Navbar() {
     return (
         <>
-            <div className={`flex items-center justify-between px-10 py-3 `}>
-
+            <div className={`flex items-center justify-between px-10 py-3 w-full bg-gray-300`}>
+                <div className={`flex items-center justify-center`}></div>
             </div>
         </>
     )
