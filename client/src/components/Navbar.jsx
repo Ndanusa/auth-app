6fc7121d2b6addea0075bb20e85bce491f27636d
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-18 14:56:06 +0100

    Navbar designing starting

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <>
+            <div className={`flex items-center justify-between px-10 py-3 `}>
+
+            </div>
+        </>
+    )
+}
+
+export default Navbar
