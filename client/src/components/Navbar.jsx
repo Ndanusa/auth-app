25a766eee2933ce141d96cbac5840266a4e9cd74
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-18 23:54:57 +0100

    serving the static files using express

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import {Settings} from 'lucide-react'
 import logoImg  from '../assets/logo.svg'
 import {useEffect, useState} from "react";
-function Navbar(){
+function Navbar(props){
+    const [userImg, setUserImg] = useState()
+    const BACKEND_URL = props.BACKEND_URL
     return (
         <>
             <div className={`flex items-center justify-between px-10 py-3 w-full bg-indigo-50`}>
@@ -18,7 +20,7 @@ function Navbar(){
                 <div className={`flex items-center justify-center`}>
                     <div className={`flex items-center justify-center gap-10`}>
                         <div className={`flex items-center justify-center w-10 h-10 bg-zinc-700 rounded-full`}>
-                            <img src={``} srcSet={``} className={`text-sm text-gray-100`} alt={`Logo`} />
+                            <img src={`${BACKEND_}`} srcSet={``} className={`text-sm text-gray-100`} alt={`Logo`} />
                         </div>
                         <div className={`cursor-pointer`} onClick={() => {}}>
                         <Settings size={23}
