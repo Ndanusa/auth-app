17432da0c8f3c97ad5871f2710d55efcc1efb64d
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-18 15:03:20 +0100

    Created the social links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,12 +3,16 @@ function Navbar() {
         <>
             <div className={`flex items-center justify-between px-10 py-3 w-full bg-gray-300`}>
                 <div className={`flex items-center justify-center`}>
-
+                    <ul className={`flex items-center justify-between gap-6 text-gray-800`}>
+                        <li><a href={'#'}>Home</a></li>
+                        <li><a href={'#'}>Feed</a></li>
+                        <li><a href={'#'}>Friends</a></li>
+                        <li><a href={'#'}>Chats</a></li>
+                        <li><a href={'#'}>Settings</a></li>
+                    </ul>
                 </div>
                 <div className={`flex items-center justify-center`}>
-                    <ul>
-                        <li><a href={'#'}>Hello</a></li>
-                    </ul>
+
                 </div>
             </div>
         </>
