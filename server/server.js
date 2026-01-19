4ac1a29fcdc03a2df131ebd098014d588152da71
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-19 14:27:27 +0100

    simple fixes

--- server/server.js
+++ server/server.js
@@ -1,25 +1,30 @@
-import {PORT, DB_URI, NODE_ENV, JWT_SECRET, JWT_EXPIRES_IN} from "./config/env.js";
+import {
+   PORT,
+   DB_URI,
+   NODE_ENV,
+   JWT_SECRET,
+   JWT_EXPIRES_IN,
+} from "./config/env.js";
 import connectDB from "./DATABASE/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import userRouter from "./routes/users.routes.js";
 import subscriptionsRouter from "./routes/subscriptions.routes.js";
 import express from "express";
-import authRouter from "./routes/auth.routes.js"
+import authRouter from "./routes/auth.routes.js";
 import cors from "cors";
 import protectedRoutes from "./routes/protected.routes.js";
 const app = express();
-app.use(cors({origin: "*"}));
-await connectDB();
-app.use(express.json())
-app.use(errorMiddleware)
+app.use(cors({ origin: "*" }));
+// await connectDB();
+app.use(express.json());
+app.use(errorMiddleware);
 app.use("/public", express.static("public"));
 //basic routes
-app.use('/api/v1/auth', protectedRoutes)
-app.use('/api/v1/auth', authRouter);
-app.use('/user', userRouter)
-app.use('/subscription', subscriptionsRouter)
-app.use(express.json())
-
+app.use("/api/v1/auth", protectedRoutes);
+app.use("/api/v1/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/subscription", subscriptionsRouter);
+app.use(express.json());
 
 app.listen(PORT, () => {
    console.log("app is listening on http://localhost:" + PORT);
