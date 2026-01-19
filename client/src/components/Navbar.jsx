3b375e133596d97afad863333f9ecd4de778441a
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-19 19:18:08 +0100

    error handling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,22 +1,21 @@
 import {
    Settings,
-   MessageCircle,
    Phone,
    Video,
    UsersRound,
+   MessageCircle,
 } from "lucide-react";
-import logoImg from "../assets/logo.svg";
+import logoImg from "../assets/logo-light.svg";
 import { useEffect, useState } from "react";
-import { BACKEND_URL } from "../config/config.js";
 function Navbar() {
    return (
       <>
-         <div className="flex flex-col py-10 px-5 h-screen bg-indigo-200 w-fit items-center justify-between">
+         <div className="flex flex-col py-10 px-5 h-screen bg-violet-950 w-fit items-center justify-between">
             <div>
                <img src={logoImg} className="w-15" alt="Logo" />
             </div>
             <div>
-               <MessageCircle color={`#0009ae`} strokeWidth={`2.5`} />
+               <MessageCircle color="#e9eaff" strokeWidth="1.6" />
                <Phone />
                <Video />
                <UsersRound />
