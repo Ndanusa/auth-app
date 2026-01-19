610f3e04f814f90118ae09de7a8b718bf9827023
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-19 15:06:48 +0100

    constructing the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-import { Settings } from "lucide-react";
+import {
+   Settings,
+   MessageCircle,
+   Phone,
+   LucideVideo,
+   UsersRoundIcon,
+} from "lucide-react";
 import logoImg from "../assets/logo.svg";
 import { useEffect, useState } from "react";
 import { BACKEND_URL } from "../config/config.js";
 function Navbar() {
-   return <></>;
+   return (
+      <>
+         <div className="flex flex-col py-10 px-5 h-screen bg-indigo-300 w-fit items-center justify-between">
+            <div>
+               <img src={logoImg} className="w-15" alt="Logo" />
+            </div>
+            <div>
+               <MessageCircle />
+               <Phone />
+               <Video />
+               <UsersRoundIcon />
+            </div>
+         </div>
+      </>
+   );
 }
 
 export default Navbar;
