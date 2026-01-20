c632338844099b51f650c3f54afb1fc34e4e8386
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-20 11:40:57 +0100

    this is a test file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import Navbar from "../components/Navbar.jsx";
 
 function Home() {
+   let count = 0;
+
    return (
       <>
          <div className="flex items-center">
             <Navbar />
+            <button className="bg-zinc-900 text-sm text-zinc-200 sqc-sm">
+               click
+            </button>
+            <div></div>
          </div>
       </>
    );
