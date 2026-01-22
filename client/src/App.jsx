905ec057e2a8d6161fe08aae727e9373686166fe
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-22 09:12:35 +0100

    testing the socket.io framework

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,64 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
-import { useEffect, useState } from "react";
-import { BACKEND_URL } from "./config/config";
-function App() {
-   const [isAuth, setIsAuth] = useState(false);
-   const [loading, setLoading] = useState(true);
-   const token = localStorage.getItem("token");
-   useEffect(() => {
-      if (!token) {
-         return setLoading(false);
-      }
-      fetch(`${BACKEND_URL}/api/v1/auth/protected`, {
-         headers: {
-            Authorization: `Bearer ${token}`,
-         },
-      })
-         .then((res) => {
-            if (!res.ok) throw new Error("Unauthorized");
-            return res.json();
-         })
-         .then(() => {
-            setIsAuth(true);
-         })
-         .catch(() => {
-            localStorage.removeItem("token");
-            setIsAuth(false);
-         })
-         .finally(() => setLoading(false));
-   }, [token]);
+// import { Routes, Route, Navigate } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import Home from "./pages/Home";
+// import { useEffect, useState } from "react";
+// import { BACKEND_URL } from "./config/config";
+// function App() {
+//    const [isAuth, setIsAuth] = useState(false);
+//    const [loading, setLoading] = useState(true);
+//    const token = localStorage.getItem("token");
+//    useEffect(() => {
+//       if (!token) {
+//          return setLoading(false);
+//       }
+//       fetch(`${BACKEND_URL}/api/v1/auth/protected`, {
+//          headers: {
+//             Authorization: `Bearer ${token}`,
+//          },
+//       })
+//          .then((res) => {
+//             if (!res.ok) throw new Error("Unauthorized");
+//             return res.json();
+//          })
+//          .then(() => {
+//             setIsAuth(true);
+//          })
+//          .catch(() => {
+//             localStorage.removeItem("token");
+//             setIsAuth(false);
+//          })
+//          .finally(() => setLoading(false));
+//    }, [token]);
 
-   if (loading) {
-      return <p>Checking Authentication...</p>;
-   }
+//    if (loading) {
+//       return <p>Checking Authentication...</p>;
+//    }
+//    return (
+//       <Routes>
+//          <Route
+//             path="/"
+//             element={<Navigate to={isAuth ? "/home" : "/login"} />}
+//          />
+//          <Route
+//             path="/login"
+//             element={isAuth ? <Navigate to={"/home"} /> : <Login />}></Route>
+//          <Route path="/signup" element={<Signup />} />
+//          <Route path="/home" element={isAuth ? <Home /> : <Login />} />
+//       </Routes>
+//    );
+// }
+
+// export default App;
+
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:3000");
+function App() {
    return (
-      <Routes>
-         <Route
-            path="/"
-            element={<Navigate to={isAuth ? "/home" : "/login"} />}
-         />
-         <Route
-            path="/login"
-            element={isAuth ? <Navigate to={"/home"} /> : <Login />}></Route>
-         <Route path="/signup" element={<Signup />} />
-         <Route path="/home" element={isAuth ? <Home /> : <Login />} />
-      </Routes>
+      <>
+         <div></div>
+      </>
    );
 }
 
