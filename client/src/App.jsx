a3881fc7c10172f7aed3508e3c59a2deec45975d
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-22 20:20:50 +0100

    get

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -51,10 +51,12 @@
 
 // export default App;
 
+import { useState } from "react";
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 const socket = io("http://localhost:3000");
 function App() {
+   const [message, setMessage] = useState("");
    useEffect(() => {
       socket.on("liveUpdate", (data) => {
          console.log("live update received: ", data);
@@ -62,15 +64,25 @@ function App() {
       return () => socket.off("liveUpdate");
    }, []);
    const handleClick = () => {
-      socket.emit("userAction", { clicked: true });
+      socket.emit("userAction", { message });
    };
    return (
       <>
-         <button
-            onClick={handleClick}
-            className="px-8 py-1 sqc-sm bg-zinc-800 text-zinc-200">
-            Click
-         </button>
+         <div>
+            <div className="flex flex-col items-start gap-5 ">
+               <input
+                  name="input"
+                  type="text"
+                  placeholder="enter your message"
+                  className="placeholder:text-sm bg-zinc-200 px-4 py-2 sqc-lg"
+               />
+               <button
+                  onClick={handleClick}
+                  className="px-8 py-1 sqc-sm bg-zinc-800 text-zinc-200">
+                  Click
+               </button>
+            </div>
+         </div>
       </>
    );
 }
