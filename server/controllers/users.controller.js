63a5f68973d6be6cab72584287e1cf86d26af481
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-24 00:09:25 +0100

    updated the user controllers

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -2,5 +2,5 @@ import User from "../models/user.models.js";
 
 export const getUsers = async (req, res) => {
    const users = await User.find().select("name username _id");
-   res.send(JSON.stringify(users));
+   res.json(users);
 };
