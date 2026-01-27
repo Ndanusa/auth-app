53a9d6c73b62a82da8cb3298542e92be06fcbd6f
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-27 23:40:20 +0100

    comrade

--- server/server.js
+++ server/server.js
@@ -5,21 +5,21 @@ import {
    JWT_SECRET,
    JWT_EXPIRES_IN,
 } from "./config/env.js";
-import connectDB from "./DATABASE/mongodb.js";
 import errorMiddleware from "./middleware/error.middleware.js";
 import userRouter from "./routes/users.routes.js";
 import subscriptionsRouter from "./routes/subscriptions.routes.js";
 import express from "express";
+import connectDB from "./DATABASE/mongodb.js";
 import authRouter from "./routes/auth.routes.js";
 import cors from "cors";
 import protectedRoutes from "./routes/protected.routes.js";
 const app = express();
 app.use(cors({ origin: "*" }));
-await connectDB();
 app.use(express.json());
 app.use(errorMiddleware);
 app.use("/public", express.static("public"));
 //basic routes
+await connectDB;
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/auth", protectedRoutes);
 app.use("/api/v1/auth", authRouter);
