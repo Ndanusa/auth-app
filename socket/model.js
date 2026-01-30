20e731f314ca0b7d13bffd70fc0cc7c22126cf60
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-01-30 21:56:13 +0100

    lodge

--- socket/model.js
+++
@@ -1,7 +0,0 @@
-async function getData() {
-   const response = await fetch("http://localhost:4400/api/v1/auth/user");
-   const data = await response.json();
-   return data;
-}
-
-export default getData;
